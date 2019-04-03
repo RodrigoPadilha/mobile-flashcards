c2033eca15b16ef58fb579cec4249bf1b90c7edf
Rodrigo <rodrigo.gts@hotmail.com>
2019-04-03 13:22:28 -0300

    Criado componente de lista

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DeckList from './components/DeckList'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Rodrigo</Text>
+        <Text>Rodrigo, Parabéns!</Text>
+        <DeckList></DeckList>
       </View>
     );
   }
